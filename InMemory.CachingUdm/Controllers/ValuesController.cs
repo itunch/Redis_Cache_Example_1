962c00751339c9e5a73ea5d453e9cf04c361ff58
Aytunc ALPTEKIN <aytunc.alptekin@beymen.com>
2024-08-05 18:34:35 +0300

    Add project files.

---
+++ InMemory.CachingUdm/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.CachingUdm.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IMemoryCache _memoryCache;
+        public ValuesController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        //[HttpGet("set/{name}")]
+        //public void SetName(string name)
+        //{
+        //    _memoryCache.Set("name",name);
+        //}
+
+        //[HttpGet]
+        //public string GetName() 
+        //{ 
+        //    if(_memoryCache.TryGetValue<string>("name",out string name))
+        //    {
+        //        return name.Substring(3);
+
+        //    }
+        //    return "";
+        //    //var name=_memoryCache.Get<string>("name");
+        //    //return name.Substring(3);
+        //}
+
+        [HttpGet("setDate")]
+        public void SetDate()
+        {
+            _memoryCache.Set<DateTime>("date", DateTime.Now, options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration=TimeSpan.FromSeconds(5)
+            });
+        }
+
+        [HttpGet("getDate")]
+        public DateTime GetDate()
+        {
+            return _memoryCache.Get<DateTime>("date");
+        }
+
+
+    }
+}
