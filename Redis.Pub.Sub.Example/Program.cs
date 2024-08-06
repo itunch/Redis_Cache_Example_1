cb9a541fe35c5b0f3cfc7e4f076916f184d8922d
Aytunc ALPTEKIN <aytunc.alptekin@beymen.com>
2024-08-06 17:29:16 +0300

    Pub-Sub example

---
+++ Redis.Pub.Sub.Example/Program.cs
@@ -0,0 +1,10 @@
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:1276");
+ISubscriber subscriber = connection.GetSubscriber();
+while (true)
+{
+    Console.WriteLine("Mesaj: ");
+    string message=Console.ReadLine();
+    await subscriber.PublishAsync("mychannel",message);
+}
